07569f2082b5d1a76c35c0177aa90b002ef9e202
rajaram2003 <rajarambiswal123@gmail.com>
2024-09-01 10:29:50 +0530

    add: initial files

---
+++ src/sections/SpecialOffer.jsx
@@ -0,0 +1,42 @@
+import { arrowRight } from "../assets/icons";
+import { offer } from "../assets/images";
+import Button from "../components/Button";
+
+const SpecialOffer = () => {
+  return (
+    <section className='flex justify-between items-center max-xl:flex-col-reverse gap-10 max-container'>
+      <div className='flex-1'>
+        <img
+          src={offer}
+          alt='Shoe Promotion'
+          width={773}
+          height={687}
+          className='object-contain w-full'
+        />
+      </div>
+      <div className='flex flex-1 flex-col'>
+        <h2 className='text-4xl font-palanquin font-bold'>
+          <span className='text-coral-red'>Special </span>
+          Offer
+        </h2>
+        <p className='mt-4 info-text'>
+        Unlock unbeatable deals on our wide range of Nike footwear with our limited-time offers! From discounts on popular styles to special promotions on new releases, our Offers section is packed with savings designed to help you get the best value for your money.
+        </p>
+        <p className='mt-6 info-text'>
+        Check back regularly to stay updated on the latest offers and grab your favorite pairs before they're gone!
+        </p>
+        <div className='mt-11 flex flex-wrap gap-4'>
+          <Button label='Shop now' iconURL={arrowRight} />
+          <Button
+            label='Learn more'
+            backgroundColor='bg-white'
+            borderColor='border-slate-gray'
+            textColor='text-slate-gray'
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SpecialOffer;
